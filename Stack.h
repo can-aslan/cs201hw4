ac9410b7121b13b457e5a0c5e50f8ed20b6c6f6a
can-aslan <yagizcanaslan0@gmail.com>
2022-01-01 16:43:16 +0300

    Added Stack.h header file

---
+++ Stack.h
@@ -0,0 +1,27 @@
+// Yağız Can Aslan 22001943
+
+#include <string>
+using namespace std;
+
+typedef string StackItem;
+
+class Stack{
+    public:
+        Stack();
+        Stack( const Stack& otherStack);
+        ~Stack();
+        
+        bool isEmpty() const;
+        bool push( StackItem newItem);
+        bool pop();
+        bool pop( StackItem& stackTop);
+        bool getTop( StackItem& stackTop) const;
+
+    private:
+        struct StackNode {
+            StackItem item;
+            StackNode* next;
+        };
+
+        StackNode *topNode;
+};
