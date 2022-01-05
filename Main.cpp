96e98e9d6ecc41c7856849f3ed1c33f0c49b5d11
can-aslan <yagizcanaslan0@gmail.com>
2022-01-05 22:08:44 +0300

    Completed proper and more thorough test driver

--- Main.cpp
+++ Main.cpp
@@ -7,19 +7,68 @@ using namespace std;
 
 int main() {
     cout << "Test started." << endl;
+    cout << endl;
 
-    cout << infix2prefix("( 12 + 5 ) - 20 * 4") << endl;
-    cout << infix2postfix("( 12 + 5 ) - 20 * 4") << endl;
-    cout << evaluateInfix("( 12 + 5 ) - 20 * 4") << endl;
+    cout << "----------------------------------------------------------" << endl;
+    cout << "Testing INFIX to PREFIX..." << endl;
+    cout << "Testing: " << "( 25 / 5 ) + ( ( 19 + 2 ) * 3 )" << endl;
+    cout << "Result:   " << infix2prefix("( 25 / 5 ) + ( ( 19 + 2 ) * 3 )") << endl;
+    cout << "Expected: " << "+ / 25 5 * + 19 2 3" << endl;
+    cout << endl;
 
-    cout << prefix2infix("* + * 100 2 4 - 12 4") << endl;
-    cout << prefix2postfix("* + * 100 2 4 - 12 4") << endl;
-    cout << evaluatePrefix("* + * 100 2 4 - 12 4") << endl;
+    cout << "Testing INFIX to POSTFIX..." << endl;
+    cout << "Testing: " << "( 25 / 5 ) + ( ( 19 + 2 ) * 3 )" << endl;
+    cout << "Result:   " << infix2postfix("( 25 / 5 ) + ( ( 19 + 2 ) * 3 )") << endl;
+    cout << "Expected: " << "25 5 / 19 2 + 3 * +" << endl;
+    cout << endl;
 
-    cout << postfix2infix("100 12 2 - 8 * + 4 /") << endl;
-    cout << postfix2prefix("100 12 2 - 8 * + 4 /") << endl;
-    cout << evaluatePostfix("100 12 2 - 8 * + 4 /") << endl;
+    cout << "Testing EVALUATE INFIX..." << endl;
+    cout << "Testing: " << "( 25 / 5 ) + ( ( 19 + 2 ) * 3 )" << endl;
+    cout << "Result:   " << evaluateInfix("( 25 / 5 ) + ( ( 19 + 2 ) * 3 )") << endl;
+    cout << "Expected: " << "68" << endl;
+    cout << "----------------------------------------------------------" << endl;
+    cout << endl;
 
-    cout << endl << "Test completed." << endl;
+    cout << "----------------------------------------------------------" << endl;
+    cout << "Testing PREFIX to INFIX..." << endl;
+    cout << "Testing: " << "+ 92 / * 16 8 4" << endl;
+    cout << "Result:   " << prefix2infix("+ 92 / * 16 8 4") << endl;
+    cout << "Expected: " << "( 92 + ( ( 16 * 8 ) / 4 ) )" << endl;
+    cout << endl;
+
+    cout << "Testing PREFIX to POSTFIX..." << endl;
+    cout << "Testing: " << "+ 92 / * 16 8 4" << endl;
+    cout << "Result:   " << prefix2postfix("+ 92 / * 16 8 4") << endl;
+    cout << "Expected: " << "92 16 8 * 4 / +" << endl;
+    cout << endl;
+
+    cout << "Testing EVALUATE PREFIX..." << endl;
+    cout << "Testing: " << "+ 92 / * 16 8 4" << endl;
+    cout << "Result:   " << evaluatePrefix("+ 92 / * 16 8 4") << endl;
+    cout << "Expected: " << "124" << endl;
+    cout << "----------------------------------------------------------" << endl;
+    cout << endl;
+
+    cout << "----------------------------------------------------------" << endl;
+    cout << "Testing POSTFIX to INFIX..." << endl;
+    cout << "Testing: " << "12 4 / 2 4 5 - + *" << endl;
+    cout << "Result:   " << postfix2infix("12 4 / 2 4 5 - + *") << endl;
+    cout << "Expected: " << "( ( 12 / 4 ) * ( 2 + ( 4 - 5 ) ) )" << endl;
+    cout << endl;
+
+    cout << "Testing POSTFIX to PREFIX..." << endl;
+    cout << "Testing: " << "12 4 / 2 4 5 - + *" << endl;
+    cout << "Result:   " << postfix2prefix("12 4 / 2 4 5 - + *") << endl;
+    cout << "Expected: " << "* / 12 4 + 2 - 4 5" << endl;
+    cout << endl;
+
+    cout << "Testing EVALUATE POSTFIX..." << endl;
+    cout << "Testing: " << "12 4 / 2 4 5 - + *" << endl;
+    cout << "Result:   " << evaluatePostfix("12 4 / 2 4 5 - + *") << endl;
+    cout << "Expected: " << "3" << endl;
+    cout << "----------------------------------------------------------" << endl;
+    cout << endl;
+
+    cout << "Test completed." << endl;
     return 0;
 }
